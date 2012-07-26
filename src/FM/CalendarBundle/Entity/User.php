6aeb09076b5e13a5512a3cdb72cd1d30a118d535
François Monniot <francoismonniot@gmail.com>
2012-07-26 20:58:46 +0200

    Configuration of fosuser and mopabootstrap

---
+++ src/FM/CalendarBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+// src/FM/UserBundle/Entity/User.php
+namespace FM\CalendarBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fmuser_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
