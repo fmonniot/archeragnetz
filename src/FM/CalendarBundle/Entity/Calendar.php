ecc04d3710d0fcae8006ca60b5f45557921d4c98
François Monniot <francoismonniot@gmail.com>
2012-07-30 16:43:33 +0200

    Generates basic entities

---
+++ src/FM/CalendarBundle/Entity/Calendar.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace FM\CalendarBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FM\CalendarBundle\Entity\Calendar
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="FM\CalendarBundle\Entity\CalendarRepository")
+ */
+class Calendar
+{
+    /**
+     *
+     * @var integer $id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     *
+     * @var string $name
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     *
+     * @var ArrayCollection $events
+     * @ORM\OneToMany(targetEntity="Event", mappedBy="calendar")
+     */
+    private $events;
+
+    /**
+     *
+     * @var datetime $created_at
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $created_at;
+
+    /**
+     *
+     * @var datetime $updated_at
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updated_at;
+
+    public function __construct()
+    {
+        $this->events = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Calendar
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param datetime $createdAt
+     * @return Calendar
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return datetime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updated_at
+     *
+     * @param datetime $updatedAt
+     * @return Calendar
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+        return $this;
+    }
+
+    /**
+     * Get updated_at
+     *
+     * @return datetime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+    
+    /**
+     * Add events
+     *
+     * @param FM\CalendarBundle\Entity\Event $events
+     * @return Calendar
+     */
+    public function addEvent(\FM\CalendarBundle\Entity\Event $events)
+    {
+        $this->events[] = $events;
+        return $this;
+    }
+
+    /**
+     * Remove events
+     *
+     * @param <variableType$events
+     */
+    public function removeEvent(\FM\CalendarBundle\Entity\Event $events)
+    {
+        $this->events->removeElement($events);
+    }
+
+    /**
+     * Get events
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getEvents()
+    {
+        return $this->events;
+    }
+}
--- src/FM/CalendarBundle/Entity/User.php
+++ src/FM/CalendarBundle/Entity/User.php
@@ -12,9 +12,98 @@ use Doctrine\ORM\Mapping as ORM;
 class User extends BaseUser
 {
     /**
+     *
+     * @var integer $id
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * Mobile Number (format +xxxxxxxxxxx)
+     *
+     * @var integer $mobile     
+     * @ORM\Column(name="mobile_number", type="integer", length=11, nullable=true)
+     */
+     private $mobile;
+     
+     /**
+      * Address
+      *
+      * @var ArrayCollection $address
+      * @ORM\ManyToMany(targetEntity="Address", inversedBy="users")
+      * @ORM\JoinTable(name="users_addresses")
+      */
+     private $addresss;
+     
+     
+    public function __construct()
+    {
+        $this->address = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param integer $mobile
+     * @return User
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return integer 
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * Add addresss
+     *
+     * @param FM\CalendarBundle\Entity\Address $addresss
+     * @return User
+     */
+    public function addAddress(\FM\CalendarBundle\Entity\Address $addresss)
+    {
+        $this->addresss[] = $addresss;
+        return $this;
+    }
+
+    /**
+     * Remove addresss
+     *
+     * @param <variableType$addresss
+     */
+    public function removeAddress(\FM\CalendarBundle\Entity\Address $addresss)
+    {
+        $this->addresss->removeElement($addresss);
+    }
+
+    /**
+     * Get addresss
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getAddresss()
+    {
+        return $this->addresss;
+    }
 }
