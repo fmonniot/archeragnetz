6051e7c089d1be5299ed3df74c611fad46341d2c
François Monniot <francoismonniot@gmail.com>
2012-08-01 17:52:30 +0200

    Real display of public event

--- src/FM/CalendarBundle/Controller/DefaultController.php
+++ src/FM/CalendarBundle/Controller/DefaultController.php
@@ -8,6 +8,11 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FMCalendarBundle:Default:index.html.twig');
+        $query = $this->getDoctrine()->getRepository('FMCalendarBundle:Calendar')->createQueryBuilder('c')
+            ->where('c.name = :name')
+            ->setParameter('name','Publique')
+            ->getQuery();
+        $events = $query->getSingleResult()->getEvents();
+        return $this->render('FMCalendarBundle:Default:index.html.twig', array('events'=>$events));
     }
 }
