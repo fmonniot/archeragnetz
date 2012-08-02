94b02b78e72d2bda82622608436162cc39b291fb
François Monniot <francoismonniot@gmail.com>
2012-08-02 20:58:24 +0200

    Display all events when connected

--- src/FM/CalendarBundle/Controller/DefaultController.php
+++ src/FM/CalendarBundle/Controller/DefaultController.php
@@ -8,11 +8,18 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $query = $this->getDoctrine()->getRepository('FMCalendarBundle:Calendar')->createQueryBuilder('c')
-            ->where('c.name = :name')
-            ->setParameter('name','Publique')
-            ->getQuery();
-        $events = $query->getSingleResult()->getEvents();
+        if( $this->get('security.context')->isGranted('ROLE_USER') )
+        {
+            $events = $this->getDoctrine()->getRepository('FMCalendarBundle:Event')->findAll();
+        }
+        else 
+        {
+            $query = $this->getDoctrine()->getRepository('FMCalendarBundle:Calendar')->createQueryBuilder('c')
+                ->where('c.name = :name')
+                ->setParameter('name','Publique')
+                ->getQuery();
+            $events = $query->getSingleResult()->getEvents();
+        }
         return $this->render('FMCalendarBundle:Default:index.html.twig', array('events'=>$events));
     }
 }
