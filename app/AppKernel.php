00af8902fbd12474b6a57c6c66e578d4bfa1573e
François Monniot <francoismonniot@gmail.com>
2012-08-19 13:50:55 +0200

    Generate initial NotificationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 
             new FM\CalendarBundle\FMCalendarBundle(),
+            new FM\NotificationBundle\FMNotificationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
