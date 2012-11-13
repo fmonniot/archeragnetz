8f0a825e5cd6b439dda173c4f824e6417e9ab319
François Monniot <francoismonniot@gmail.com>
2012-11-13 18:56:05 +0100

    Create routing and associate controller

--- src/FM/ArcherAgnetzBundle/Controller/DefaultController.php
+++ src/FM/ArcherAgnetzBundle/Controller/DefaultController.php
@@ -10,4 +10,29 @@ class DefaultController extends Controller
     {
         return $this->render('FMArcherAgnetzBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function homeAction()
+    {
+        return $this->render('FMArcherAgnetzBundle:Default:index.html.twig', array('name' => 'Accueil'));
+    }
+
+    public function clubLifeAction()
+    {
+        return $this->render('FMArcherAgnetzBundle:Default:index.html.twig', array('name' => 'Vie du club'));
+    }
+
+    public function photosAction()
+    {
+        return $this->render('FMArcherAgnetzBundle:Default:index.html.twig', array('name' => 'Galerie Photo'));
+    }
+
+    public function joinUsAction()
+    {
+        return $this->render('FMArcherAgnetzBundle:Default:index.html.twig', array('name' => 'Nous rejoindre'));
+    }
+
+    public function contactAction()
+    {
+        return $this->render('FMArcherAgnetzBundle:Default:index.html.twig', array('name' => 'Nous contacter'));
+    }
 }
